099a4914d61959a40db7ecd195f890ec3c8247a3
Amoljamnik97 <ajamnik.mech@gmail.com>
2022-11-24 10:27:40 +0530

    Prime Number in that Range

--- forloop.sh
+++ forloop.sh
@@ -1,45 +1,15 @@
-echo "Enter a number"
-
-read num
-
-if [ $num -eq 1 ]
-
-then
-
-echo "1 is not a prime number"
-
-else
-
-i=2
-
-count=0
-
-while [ $i -lt $num ]
-
+for num in {1..100}
 do
-
-if [ `expr $num % $i` -eq 0 ]
-
-then
-
-count=1
-
-fi
-
-i=`expr $i + 1`
-
+	count=0
+	for((i=2;i<=num;i++))
+	do
+		if((($num%$i)==0))
+		then
+			((count++))
+		fi
+	done
+	if((count==1))
+	then
+		echo $num
+	fi
 done
-
-if [ $count -eq 0 ]
-
-then
-
-echo "$num is a prime number"
-
-else
-
-echo "$num is not a prime number"
-
-fi
-
-fi
