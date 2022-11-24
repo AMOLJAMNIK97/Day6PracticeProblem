9e84ea04551904ec73df04d75da0e83947c2ff55
Amoljamnik97 <ajamnik.mech@gmail.com>
2022-11-24 10:32:32 +0530

    Add: Factorial Number

--- forloop.sh
+++ forloop.sh
@@ -1,15 +1,11 @@
-for num in {1..100}
+read -p "Enter a number: " num
+
+factorial=1
+
+for (( i=1; i<=num; i++ ))
 do
-	count=0
-	for((i=2;i<=num;i++))
-	do
-		if((($num%$i)==0))
-		then
-			((count++))
-		fi
-	done
-	if((count==1))
-	then
-		echo $num
-	fi
+    factorial=$(( factorial * i ))
 done
+
+echo "Factorial of $num: $factorial"
+
